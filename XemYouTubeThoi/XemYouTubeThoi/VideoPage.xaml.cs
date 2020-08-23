7ea3e164b4c5e16c406e5df8a3ce91d54101160e
Tranthanhhai281 <Tranthanhhai281@gmail.com>
2020-08-23 17:32:27 +0700

    UWP tutorial 9,10

---
+++ XemYouTubeThoi/XemYouTubeThoi/VideoPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace XemYouTubeThoi
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class VideoPage : Page
+    {
+        Video video;
+        public VideoPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected async override void OnNavigateTo(NavigationEventArgs e)
+        {
+            try
+            {
+                if (Networkinterface.GetIsNetworkAvailable())
+                {
+                    video = e.Parameter as Video;
+                    var Url = await YouTube.GetVideoUriAsync(video.Id, YouTubeQuality.Quality1080p);
+                    Player.Source = Url.Uri;
+                }
+                else
+                {
+                    MessageDialog message = new MessageDialog("You Are Not Connected to Internet");
+                    await message.ShowAsync();
+                    this.Frame.GoBack();
+                }
+            }
+            catch { }
+            base.OnNavigatedTo(e);
+        }
+
+        private void btnHomePage_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage), new object());
+        }
+    }
+}
