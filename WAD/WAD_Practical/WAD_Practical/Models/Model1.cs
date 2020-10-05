f3c5a6253895fd0ef389857ebe62ed027503b5b4
Tranthanhhai281 <53221090+Tranthanhhai281@users.noreply.github.com>
2020-10-05 10:51:30 +0700

    WAD

---
+++ WAD/WAD_Practical/WAD_Practical/Models/Model1.cs
@@ -0,0 +1,28 @@
+namespace WAD_Practical.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Contacts> Contacts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contacts>()
+                .Property(e => e.ContactName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Contacts>()
+                .Property(e => e.GroupName)
+                .IsUnicode(false);
+        }
+    }
+}
